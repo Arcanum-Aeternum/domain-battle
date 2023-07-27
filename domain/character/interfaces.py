b10f7d40eae12167f2fcafa8eac910baddcff8ab
MrAbade <vini.abade@outlook.com.br>
2023-07-27 01:16:14 -0300

    refactor(#7): segregate interface with public methods
    feature/creates-balanced-or-unbalanced-battles-with-any-number-of-players

--- domain/character/interfaces.py
+++ domain/character/interfaces.py
@@ -1,49 +1,65 @@
 from abc import ABCMeta, abstractmethod
+from typing import Generator
 
+from domain.character_combat_technique import ICharacterCombatTechniquePublicMethods
+from domain.character_spell import ICharacterSpellPublicMethods
 from domain.interfaces import IEntityID
 
-from .ext_interfaces import IExpectedCharacterCombatTechnique, IExpectedCharacterSpell
 
+class ICharacterPublicMethods(metaclass=ABCMeta):
+    """Interface that defines the public methods in Character"""
 
-class ICharacter(metaclass=ABCMeta):
-    """Interface that define the public methods of Character with exception of factory methods"""
-
-    entity_id: IEntityID
-
+    @property
     @abstractmethod
-    def create_new(self, *, entity_id: IEntityID, name: str) -> "ISkillProfileBuilder":
+    def name(self) -> str:
         ...
 
     @property
     @abstractmethod
-    def get_name(self) -> str:
+    def is_alive(self) -> bool:
         ...
 
     @property
     @abstractmethod
-    def get_current_stamina_points(self) -> int:
+    def current_stamina_points(self) -> int:
         ...
 
     @property
     @abstractmethod
-    def get_current_mana_points(self) -> int:
+    def current_mana_points(self) -> int:
         ...
 
     @property
     @abstractmethod
-    def get_current_life_points(self) -> int:
+    def current_life_points(self) -> int:
+        ...
+
+    @property
+    @abstractmethod
+    def available_combat_techniques(self) -> Generator[ICharacterCombatTechniquePublicMethods, None, None]:
+        ...
+
+    @property
+    @abstractmethod
+    def available_spells(self) -> Generator[ICharacterSpellPublicMethods, None, None]:
         ...
 
     @abstractmethod
-    def apply_combat_technique(self, *, combat_technique_id: IEntityID, target_character: "ICharacter") -> None:
+    def apply_combat_technique(
+        self, combat_technique_id: IEntityID, target_character: "ICharacterPublicMethods"
+    ) -> None:
         ...
 
     @abstractmethod
-    def cast_spell(self, *, spell_id: IEntityID, target_character: "ICharacter") -> None:
+    def cast_spell(self, spell_id: IEntityID, target_character: "ICharacterPublicMethods") -> None:
         ...
 
     @abstractmethod
-    def receive_attack(self, damage: int) -> None:
+    def rest(self) -> None:
+        ...
+
+    @abstractmethod
+    def _receive_attack(self, damage: int) -> None:
         ...
 
 
@@ -51,15 +67,15 @@ class ISpellsBuilder(metaclass=ABCMeta):
     """Interface that defines an easy way to add spells to the Character"""
 
     @abstractmethod
-    def build(self) -> ICharacter:
+    def build(self) -> ICharacterPublicMethods:
         ...
 
     @abstractmethod
-    def add_spells(self, *spells: IExpectedCharacterSpell) -> ICharacter:
+    def add_spells(self, *spells: ICharacterSpellPublicMethods) -> ICharacterPublicMethods:
         ...
 
     @abstractmethod
-    def add_spell(self, spell: IExpectedCharacterSpell) -> "ISpellsBuilder":
+    def add_spell(self, spell: ICharacterSpellPublicMethods) -> "ISpellsBuilder":
         ...
 
 
@@ -71,11 +87,13 @@ class ICombatTechniquesBuilder(metaclass=ABCMeta):
         ...
 
     @abstractmethod
-    def add_combat_techniques(self, *combat_techniques: IExpectedCharacterCombatTechnique) -> ISpellsBuilder:
+    def add_combat_techniques(self, *combat_techniques: ICharacterCombatTechniquePublicMethods) -> ISpellsBuilder:
         ...
 
     @abstractmethod
-    def add_combat_technique(self, combat_technique: IExpectedCharacterCombatTechnique) -> "ICombatTechniquesBuilder":
+    def add_combat_technique(
+        self, combat_technique: ICharacterCombatTechniquePublicMethods
+    ) -> "ICombatTechniquesBuilder":
         ...
 
 
@@ -84,6 +102,17 @@ class ISkillProfileBuilder(metaclass=ABCMeta):
 
     @abstractmethod
     def specify_skill_properties(
-        self, *, life_points: int, stamina_points: int, mana_points: int
+        self, life_points: int, stamina_points: int, mana_points: int
     ) -> ICombatTechniquesBuilder:
+        ...
+
+
+class ICharacter(metaclass=ABCMeta):
+    """Interface that define the public methods of Character with exception of factory methods"""
+
+    entity_id: IEntityID
+
+    @classmethod
+    @abstractmethod
+    def create_new(cls, *, entity_id: IEntityID, name: str) -> ISkillProfileBuilder:
         ...
