9cb33a5855a5b34ab112708f4060f0c27c1e3aa7
MrAbade <vini.abade@outlook.com.br>
2023-08-01 13:32:03 -0300

    feat(#7): add exceptions when the ability is not available
    feature/creates-balanced-or-unbalanced-battles-with-any-number-of-players

--- domain/character/exceptions.py
+++ domain/character/exceptions.py
@@ -4,3 +4,11 @@ class ThisCharacterDoesNotHaveThatSpell(RuntimeError):
 
 class ThisCharacterDoesNotHaveThatCombatTechnique(RuntimeError):
     """Error that indicates that the character does not have the combat technique he tried to use"""
+
+
+class CombatTechniqueNotAvailable(RuntimeError):
+    """Error that indicates that the requested combat technique is on cooldown"""
+
+
+class SpellNotAvailable(RuntimeError):
+    """Error that indicates that the requested spell is on cooldown"""
